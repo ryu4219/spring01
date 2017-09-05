0f63ccb2d200fd4e30b2c6cd44272b17a9d063a9
java <java@bigdata>
2017-09-05 16:10:28 +0900

    scala-maven-plugin add

---
+++ src/main/scala/com/example/DeptExample.scala
@@ -0,0 +1,15 @@
+package com.example
+
+object DeptExample {
+    def main(args: Array[String]): Unit = {
+        
+        var dept = new Dept
+        dept.deptno = 10
+        dept.dname = "총무부"
+        dept.loc = "서울"
+        
+        println("deptno = " + dept.deptno);
+        println("dname = " + dept.dname);
+        println("loc = " + dept.loc);
+    }
+}
