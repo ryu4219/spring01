0f3c62f87df2888cf7a29ee41b086bf49c141ff4
java <java@bigdata>
2017-09-05 15:21:43 +0900

    java 1.5 -> 1.8

--- src/main/java/com/example/HelloSpring.java
+++ src/main/java/com/example/HelloSpring.java
@@ -2,8 +2,12 @@ package com.example;
 
 public class HelloSpring {
 
-	public static void main(String[] args) {
+   public static void main(String[] args) {
 
-	}
+      System.out.println("Spring의 두가지 추상화된 설계");
+      System.out.println("1. DI(Dependency Injection)");
+      System.out.println("2. AOP(Aspect Objected Programming)관점 지향 프로그래밍");
+      
+   }
 
 }
